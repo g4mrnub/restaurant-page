d46e073ab8ac26a05df04a5913d4b32e142f2aea
Aaron <g4mrnub@gmail.com>
2023-08-03 18:26:37 -0600

    add tab switch functionality

--- src/index.js
+++ src/index.js
@@ -3,23 +3,42 @@ import homePage from './homepage';
 import menuPage from './menupage';
 import aboutPage from './aboutpage';
 
-
+function switchTab(tab){
+    //remove the current page
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    //add the clicked tab's content to the page
+    content.append(tab);
+}
 
 const body = document.querySelector("body");
 
 const navbar = document.createElement("div");
 navbar.classList.add("navbar");
 
+const content = document.createElement("main");
+
 const home = document.createElement("a");
 home.textContent = "home";
+home.addEventListener('click', ()=>{
+    switchTab(homePage())
+});
+
 const menu = document.createElement("a");
 menu.textContent = "menu";
+menu.addEventListener('click', ()=>{
+    switchTab(menuPage())
+});
 const idk = document.createElement("a");
 idk.textContent = "idk";
-const brunch = document.createElement("a");
-brunch.textContent = "brunch";
-const specials = document.createElement("a");
-specials.textContent = "specials";
+
+const contact = document.createElement("a");
+contact.textContent = "contact";
+contact.addEventListener('click', ()=>{
+    switchTab(aboutPage())
+});
+
 
 
 const footer = document.createElement("div");
@@ -27,6 +46,7 @@ footer.classList.add("footer");
 const footerContent = document.createElement("h2");
 footerContent.textContent = "www.Instagram.com/JoeSupreme";
 
-body.append(navbar, homePage(), footer);
-navbar.append(home, menu, idk, brunch, specials);
+body.append(navbar, content, footer);
+switchTab(homePage());
+navbar.append(home, menu, idk, contact);
 footer.appendChild(footerContent);
