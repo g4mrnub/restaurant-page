15b51842a7b9b30dd34c2ae774ccbd803c2de680
Aaron <g4mrnub@gmail.com>
2023-08-07 16:45:07 -0600

    enclose index code in functions

--- src/index.js
+++ src/index.js
@@ -12,41 +12,47 @@ function switchTab(tab){
     content.append(tab);
 }
 
-const body = document.querySelector("body");
+function createPage(){
+    const body = document.querySelector("body");
 
-const navbar = document.createElement("div");
-navbar.classList.add("navbar");
+    const navbar = document.createElement("div");
+    navbar.classList.add("navbar");
+    
+    const content = document.createElement("main");
+    
+    const home = document.createElement("a");
+    home.textContent = "home";
+    home.addEventListener('click', ()=>{
+        switchTab(homePage())
+    });
+    
+    const menu = document.createElement("a");
+    menu.textContent = "menu";
+    menu.addEventListener('click', ()=>{
+        switchTab(menuPage())
+    });
+    const idk = document.createElement("a");
+    idk.textContent = "idk";
+    
+    const contact = document.createElement("a");
+    contact.textContent = "contact";
+    contact.addEventListener('click', ()=>{
+        switchTab(aboutPage())
+    });
+    
+    const footer = document.createElement("div");
+    footer.classList.add("footer");
+    const footerContent = document.createElement("h2");
+    footerContent.textContent = "www.Instagram.com/JoeSupreme";
 
-const content = document.createElement("main");
+    body.append(navbar, content, footer);
+    switchTab(homePage());
+    navbar.append(home, menu, idk, contact);
+    footer.appendChild(footerContent);
+}
 
-const home = document.createElement("a");
-home.textContent = "home";
-home.addEventListener('click', ()=>{
-    switchTab(homePage())
-});
 
-const menu = document.createElement("a");
-menu.textContent = "menu";
-menu.addEventListener('click', ()=>{
-    switchTab(menuPage())
-});
-const idk = document.createElement("a");
-idk.textContent = "idk";
-
-const contact = document.createElement("a");
-contact.textContent = "contact";
-contact.addEventListener('click', ()=>{
-    switchTab(aboutPage())
-});
+createPage();
 
 
 
-const footer = document.createElement("div");
-footer.classList.add("footer");
-const footerContent = document.createElement("h2");
-footerContent.textContent = "www.Instagram.com/JoeSupreme";
-
-body.append(navbar, content, footer);
-switchTab(homePage());
-navbar.append(home, menu, idk, contact);
-footer.appendChild(footerContent);
